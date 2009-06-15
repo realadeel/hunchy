475861a8543f57a477ad15254c20148d7e2a093b
Adeel Ahmad <adeel@adeel-ahmads-macbook-pro.local>
2009-06-15 07:59:17 -0700

    remaining available API calls as of this morning

--- lib/hunchy.rb
+++ lib/hunchy.rb
@@ -18,8 +18,29 @@ class Hunchy
     get('/getResponse', :query => {:responseId => id})
   end
 
+  # This will return question search results for the query you give it.
   def self.searchForQuestion(query)
     get('/searchForQuestion', :query => {:query => query})
   end
+  
+  # This will return statistical information pertaining to a given THAY Hunch response.
+  def self.responseStats(id)
+    get('/responseStats', :query => {:responseId => id})
+  end
+  
+  # This will return statistical information pertaining to the relationship between two THAY Hunch response.
+  def self.responsePairStats(id1, id2)
+    get('/responsePairStats', :query => {:response1Id => id1, :response2Id => id2})
+  end
 
+  # This will return the THAY responses most strongly positively correlated to a given THAY response.
+  def self.responsePositiveCorrelations(id, n)
+    get('/responsePositiveCorrelations', :query => {:responseId => id, :n => n})
+  end
+
+  # This will return the THAY responses most strongly negatively correlated to a given THAY response.
+  def self.responseNegativeCorrelations(id, n)
+    get('/responseNegativeCorrelations', :query => {:responseId => id, :n => n})
+  end
+  
 end
